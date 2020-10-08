ea0340635bba0fccbbe28be455aac8e133658649
minokun4869 <62930566+minokun4869@users.noreply.github.com>
2020-10-08 18:02:40 +0700

    Library for motor

---
+++ kmotor/kmotor.h
@@ -0,0 +1,15 @@
+#ifndef kmotor_h
+#define kmotor_H
+#include <Arduino.h>
+class kmotor
+{
+  public:
+   kmotor(bool msg);
+  void cauhinh();
+  void tien(int a,int b);
+  void run(int a,int b);
+  void stop();
+  private:
+  bool _msg;
+};
+#endif
